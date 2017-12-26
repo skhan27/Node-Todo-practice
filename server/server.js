4fe30876032eb0b2618fed30f33d9ca66db643ca
Saim Khan <saim2028@hotmail.com>
2017-12-26 01:21:29 -0500

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {ObjectID} = require('mongodb')
 
 var app = express();
 
@@ -27,6 +27,24 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    //validate id
+    if (!ObjectID.isValid(id)){
+        res.status(400).send('Invalid ID');
+    }
+    //findbyid success, error 
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send(`${id} not found`);
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.status(400).send()
     });
 });
 
